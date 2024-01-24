7e26c00a2524d2f3fe8f8d5b473224d7a134ad46
Maiwand Kakar <en.maiwand@gmail.com>
2024-01-24 19:01:15 +0300

    Need to come back to this code, it is not completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+puts "Seeds are starting"
+10.times do
+new_resturant = Restaurant.new(name: Faker::Name.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: ["chinese", "italian", "japanese", "french", "belgian"].sample )
+new_resturant.save!
+end
+
+puts "Seeds are end"
