2b5d4ccaea85e8a2f644ccaa12f1188177ea3af5
Maiwand Kakar <en.maiwand@gmail.com>
2024-01-25 08:55:23 +0300

    New code has added, but it needs to be improved.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,18 +9,21 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
-
+    @review = Review.new
   end
+
+  private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
-
-
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,4 @@
 class ReviewsController < ApplicationController
-
-
-
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
@@ -10,11 +7,15 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-    raise
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
 
   def review_params
     params.require(:review).permit(:content, :rating)
